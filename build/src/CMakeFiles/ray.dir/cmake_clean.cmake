fbe78ce92115520bab4fa99f5ea51c64434d32c8
Michael Asper <asper@utexas.edu>
2019-02-03 21:04:18 -0600

    pushed

---
+++ build/src/CMakeFiles/ray.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray.dir/RayTracer.cpp.o"
+  "CMakeFiles/ray.dir/main.cpp.o"
+  "CMakeFiles/ray.dir/fileio/bitmap.cpp.o"
+  "CMakeFiles/ray.dir/fileio/buffer.cpp.o"
+  "CMakeFiles/ray.dir/fileio/images.cc.o"
+  "CMakeFiles/ray.dir/fileio/pngimage.cpp.o"
+  "CMakeFiles/ray.dir/parser/Parser.cpp.o"
+  "CMakeFiles/ray.dir/parser/ParserException.cpp.o"
+  "CMakeFiles/ray.dir/parser/Token.cpp.o"
+  "CMakeFiles/ray.dir/parser/Tokenizer.cpp.o"
+  "CMakeFiles/ray.dir/scene/bbox.cc.o"
+  "CMakeFiles/ray.dir/scene/camera.cpp.o"
+  "CMakeFiles/ray.dir/scene/cubeMap.cpp.o"
+  "CMakeFiles/ray.dir/scene/light.cpp.o"
+  "CMakeFiles/ray.dir/scene/material.cpp.o"
+  "CMakeFiles/ray.dir/scene/ray.cpp.o"
+  "CMakeFiles/ray.dir/scene/scene.cpp.o"
+  "CMakeFiles/ray.dir/SceneObjects/Box.cpp.o"
+  "CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o"
+  "CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o"
+  "CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o"
+  "CMakeFiles/ray.dir/SceneObjects/Square.cpp.o"
+  "CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o"
+  "CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o"
+  "CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o"
+  "CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o"
+  "CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o"
+  "CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o"
+  "CMakeFiles/ray.dir/ui/TraceUI.cc.o"
+  "CMakeFiles/ray.dir/ui/debuggingView.cpp.o"
+  "CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o"
+  "CMakeFiles/ray.dir/ui/glObjects.cpp.o"
+  "../bin/ray.pdb"
+  "../bin/ray"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
