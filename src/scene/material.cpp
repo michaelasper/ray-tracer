ef1f54f818d86db79d736b785da5134cb9a1b5f8
Michael Asper <asper@utexas.edu>
2019-02-03 23:00:27 -0600

    lmfao

--- src/scene/material.cpp
+++ src/scene/material.cpp
@@ -11,121 +11,112 @@ extern TraceUI* traceUI;
 using namespace std;
 extern bool debugMode;
 
-Material::~Material()
-{
-}
+Material::~Material() {}
 
 // Apply the phong model to this point on the surface of the object, returning
 // the color of that point.
-glm::dvec3 Material::shade(Scene* scene, const ray& r, const isect& i) const
-{
-	// YOUR CODE HERE
 
-	// For now, this method just returns the diffuse color of the object.
-	// This gives a single matte color for every distinct surface in the
-	// scene, and that's it.  Simple, but enough to get you started.
-	// (It's also inconsistent with the phong model...)
+// YOUR CODE HERE
 
-	// Your mission is to fill in this method with the rest of the phong
-	// shading model, including the contributions of all the light sources.
-	// You will need to call both distanceAttenuation() and
-	// shadowAttenuation()
-	// somewhere in your code in order to compute shadows and light falloff.
-	//	if( debugMode )
-	//		std::cout << "Debugging Phong code..." << std::endl;
+// For now, this method just returns the diffuse color of the object.
+// This gives a single matte color for every distinct surface in the
+// scene, and that's it.  Simple, but enough to get you started.
+// (It's also inconsistent with the phong model...)
 
-	// When you're iterating through the lights,
-	// you'll want to use code that looks something
-	// like this:
-	//
-	// for ( const auto& pLight : scene->getAllLights() )
-	// {
-	//              // pLight has type unique_ptr<Light>
-	// 		.
-	// 		.
-	// 		.
-	// }
+// Your mission is to fill in this method with the rest of the phong
+// shading model, including the contributions of all the light sources.
+// You will need to call both distanceAttenuation() and
+// shadowAttenuation()
+// somewhere in your code in order to compute shadows and light falloff.
+//	if( debugMode )
+//		std::cout << "Debugging Phong code..." << std::endl;
 
-	double alpha = shininess(i);
-	
-	glm::dvec3 normal = glm::normalize(i.getN());
-	glm::dvec3 v = -r.getDirection();
-	if(v != glm::dvec3(0.0,0.0,0.0))
-		v = glm::normalize(v);
+// When you're iterating through the lights,
+// you'll want to use code that looks something
+// like this:
+//
+// for ( const auto& pLight : scene->getAllLights() )
+// {
+//              // pLight has type unique_ptr<Light>
+// 		.
+// 		.
+// 		.
+// }
+glm::dvec3 Material::shade(Scene* scene, const ray& r, const isect& i) const {
+    double alpha = shininess(i);
 
-	glm::dvec3 Ia = scene->ambient();
-	glm::dvec3 intensity = ke(i) + ka(i) * Ia;
+    glm::dvec3 normal = glm::normalize(i.getN());
+    glm::dvec3 v = -r.getDirection();
+    if (v != glm::dvec3(0.0, 0.0, 0.0)) v = glm::normalize(v);
+    glm::dvec3 intensity = ke(i) + ka(i) * scene->ambient();
 
-	for(const auto& pLight : scene->getAllLights()) {
-		glm::dvec3 Iin = pLight->getColor();
+    for (const auto& pLight : scene->getAllLights()) {
+        glm::dvec3 l = glm::normalize(pLight->getDirection(r.at(i.getT())));
+        glm::dvec3 reflect =
+            glm::normalize(2 * glm::dot(l, normal) * normal - l);
 
-		// Distance Attenuation
-		Iin *= min(1.0, pLight->distanceAttenuation(r.at(i.getT())));
+        // ray shadow(r.at(i.getT()) + l * 1e-4, l, r.getAtten(), ray::SHADOW);
 
-		glm::dvec3 l = glm::normalize(pLight->getDirection(r.at(i.getT())));
-		glm::dvec3 reflect = glm::normalize(2*glm::dot(l, normal)*normal - l);
-		
-		// Phong Model
-		intensity += Iin * (kd(i) * max(glm::dot(l, normal), 0.0) + ks(i) * pow(max(glm::dot(v, reflect), 0.0), alpha));
-	}
-	
-	return intensity;
+        glm::dvec3 atten =
+            pLight->getColor() *
+            min(1.0, pLight->distanceAttenuation(r.at(i.getT()))) *
+            pLight->shadowAttenuation(r, r.at(i.getT()));
+
+        // Phong Model
+        intensity +=
+            atten * (kd(i) * max(glm::dot(l, normal), 0.0) +
+                     ks(i) * pow(max(glm::dot(v, reflect), 0.0), alpha));
+    }
+
+    return intensity;
 }
 
-TextureMap::TextureMap(string filename)
-{
-	data = readImage(filename.c_str(), width, height);
-	if (data.empty()) {
-		width = 0;
-		height = 0;
-		string error("Unable to load texture map '");
-		error.append(filename);
-		error.append("'.");
-		throw TextureMapException(error);
-	}
+TextureMap::TextureMap(string filename) {
+    data = readImage(filename.c_str(), width, height);
+    if (data.empty()) {
+        width = 0;
+        height = 0;
+        string error("Unable to load texture map '");
+        error.append(filename);
+        error.append("'.");
+        throw TextureMapException(error);
+    }
 }
 
-glm::dvec3 TextureMap::getMappedValue(const glm::dvec2& coord) const
-{
-	// YOUR CODE HERE
-	//
-	// In order to add texture mapping support to the
-	// raytracer, you need to implement this function.
-	// What this function should do is convert from
-	// parametric space which is the unit square
-	// [0, 1] x [0, 1] in 2-space to bitmap coordinates,
-	// and use these to perform bilinear interpolation
-	// of the values.
+glm::dvec3 TextureMap::getMappedValue(const glm::dvec2& coord) const {
+    // YOUR CODE HERE
+    //
+    // In order to add texture mapping support to the
+    // raytracer, you need to implement this function.
+    // What this function should do is convert from
+    // parametric space which is the unit square
+    // [0, 1] x [0, 1] in 2-space to bitmap coordinates,
+    // and use these to perform bilinear interpolation
+    // of the values.
 
-	return glm::dvec3(1, 1, 1);
+    return glm::dvec3(1, 1, 1);
 }
 
-glm::dvec3 TextureMap::getPixelAt(int x, int y) const
-{
-	// YOUR CODE HERE
-	//
-	// In order to add texture mapping support to the
-	// raytracer, you need to implement this function.
+glm::dvec3 TextureMap::getPixelAt(int x, int y) const {
+    // YOUR CODE HERE
+    //
+    // In order to add texture mapping support to the
+    // raytracer, you need to implement this function.
 
-	return glm::dvec3(1, 1, 1);
+    return glm::dvec3(1, 1, 1);
 }
 
-glm::dvec3 MaterialParameter::value(const isect& is) const
-{
-	if (0 != _textureMap)
-		return _textureMap->getMappedValue(is.getUVCoordinates());
-	else
-		return _value;
+glm::dvec3 MaterialParameter::value(const isect& is) const {
+    if (0 != _textureMap)
+        return _textureMap->getMappedValue(is.getUVCoordinates());
+    else
+        return _value;
 }
 
-double MaterialParameter::intensityValue(const isect& is) const
-{
-	if (0 != _textureMap) {
-		glm::dvec3 value(
-		        _textureMap->getMappedValue(is.getUVCoordinates()));
-		return (0.299 * value[0]) + (0.587 * value[1]) +
-		       (0.114 * value[2]);
-	} else
-		return (0.299 * _value[0]) + (0.587 * _value[1]) +
-		       (0.114 * _value[2]);
+double MaterialParameter::intensityValue(const isect& is) const {
+    if (0 != _textureMap) {
+        glm::dvec3 value(_textureMap->getMappedValue(is.getUVCoordinates()));
+        return (0.299 * value[0]) + (0.587 * value[1]) + (0.114 * value[2]);
+    } else
+        return (0.299 * _value[0]) + (0.587 * _value[1]) + (0.114 * _value[2]);
 }
